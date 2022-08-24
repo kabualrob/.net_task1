a8eaa6f204ec203304c3b738ab86e3e230c03ba2
Ahmad Shtawi <104018910+ahmadshtawi@users.noreply.github.com>
2022-08-24 15:54:17 +0300

    Add files via upload

---
+++ FootballPlayer.cs
@@ -0,0 +1,82 @@
+﻿using Amazon.Runtime.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ahmed.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayerController : ControllerBase
+    {
+        private string club;
+
+        private static List<FootballPlayer> player = new List<FootballPlayer>()
+            {
+                new FootballPlayer{id=1,name="messi",Fname="leonel",Lname="messi",Club="FCB"},
+
+                 new FootballPlayer{id=2,name="xavi",Fname="noidea",Lname="xavi",Club="FCB"}
+
+            };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+
+
+            return Ok(player);
+        }
+        [HttpGet("id")]
+        public IActionResult Get(int id)
+        {
+
+            var playerr = player.Find(p => p.id == id);
+            if (playerr == null)
+                return BadRequest("player not found");
+
+            return Ok(player);
+        }
+
+
+        [HttpPost]
+        public IActionResult Addplayer(FootballPlayer playerr)
+
+        {
+            player.Add(playerr);
+
+            return Ok(player);
+        }
+        [HttpPut]
+        public IActionResult updateplayer(FootballPlayer request)
+
+        {
+            var playerr = player.Find(p => p.id == request.id);
+            if (playerr == null)
+                return BadRequest("player not found");
+
+            playerr.name = request.name;
+            playerr.Fname = request.Fname;
+            playerr.Lname = request.Lname;
+            playerr.Club = request.Club;
+
+            return Ok(player);
+
+        }
+        [HttpDelete("id")]
+        public IActionResult Delete (int id)
+        {
+
+            var playerr = player.Find(p => p.id == id);
+            if (playerr == null)
+                return BadRequest("player not found");
+            player.Remove(playerr);
+
+            return Ok(player);
+        }
+    }  
+
+
+    
+
+}
